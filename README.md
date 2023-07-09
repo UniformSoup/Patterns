8ebcd104b9798d73e02780285d7eed0c8457eb04
UniformSoup <halad@outlook.com>
2023-07-09 13:52:31 +0100

    update and add readme

---
+++ README.md
@@ -0,0 +1,5 @@
+# Patterns
+
+A template for GoF patterns, to be added as a submodule when I need them.
+
+[https://web.archive.org/web/20151002171119/http://codewrangler.home.comcast.net/~codewrangler/tech_info/patterns_code.html](GoF Website)
--- State.hpp
+++ State.hpp
@@ -3,15 +3,14 @@
 #include <memory>
 
 // Remember to give StateType a virtual destructor
+// This will make sure that deleting the StateType* calls the destructor for the derived class too.
 template <typename StateType>
 class StateMachine
 {
     std::unique_ptr<StateType> currentState;
-    std::unique_ptr<StateType> nextState = nullptr;
 
 public:
     StateMachine(std::unique_ptr<StateType>&& state) : currentState(std::move(state)) {}
     void transition(std::unique_ptr<StateType>&& newState) { nextState = std::move(newState); }
-    void update() { if (nextState != nullptr) { currentState = std::move(nextState); nextState = nullptr; } }
     StateType& state() { return *currentState; }
 };
